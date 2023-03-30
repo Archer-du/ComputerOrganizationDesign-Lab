c5378871e9102da7ec7aa2ba71e06a37ad415b21
Archer <dupengche@gmail.com>
2023-03-31 00:02:35 +0800

    alu unit added

--- COD/LAB1/modules/FLS.v
+++ COD/LAB1/modules/FLS.v
@@ -5,10 +5,12 @@ module FLS
         input [6:0] d,
         output [6:0] f
     );
-    //reg en_0=1'b1,en_1=1'b1;
-    reg [1:0] cs=2'b00,ns;//  state=={en_0,en_1}
-    reg [6:0] f_0,f_1,F;
     //初始化
+    reg [1:0] cs=2'b00,ns;      //state=={en_0,en_1}
+    reg [6:0] f_0,f_1;
+    wire [6:0] F;
+    alu_unit #(7) alu(.a(f_0),.b(f_1),.func(4'b0000),.y(F));
+    //双寄存器构造单周期脉冲
     reg r1=0,r2=0;
     wire pulse;
     always@(posedge clk)
@@ -16,8 +18,8 @@ module FLS
     always@(posedge clk)
     r2<=r1;
     assign pulse=r1&(~r2);
-    //双寄存器构造单周期脉冲
     // --------------------------------------------
+    //状态转换模块
     always@(posedge clk)
     begin
         if(rst)
@@ -29,12 +31,11 @@ module FLS
         else if(pulse)
         begin
             cs<=ns;
-            case(ns)
-            2'b10:f_0<=d;
-            2'b01:f_1<=d;
+            case(cs)
+            2'b00:f_0<=d;
+            2'b10:f_1<=d;
             2'b11:
             begin
-                F=f_0+f_1;
                 f_0<=f_1;
                 f_1<=F;
             end
@@ -42,6 +43,7 @@ module FLS
         end
     end
     // --------------------------------------------
+    //状态激励模块
     always@(*)
     begin
         case(cs)
@@ -52,5 +54,7 @@ module FLS
         endcase
     end
     // --------------------------------------------
+    //组合输出模块
     assign f=(cs==2'b11)?F:((cs==2'b10)?f_0:((cs==2'b01)?f_1:7'b0));
+    // --------------------------------------------
 endmodule
