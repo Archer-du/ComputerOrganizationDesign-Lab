e633c52c06aaddd702c3ec187d2036d71647a696
Archer <dupengche@gmail.com>
2023-04-10 15:26:05 +0800

    update comments

--- COD/LAB2/modules/LCU.v
+++ COD/LAB2/modules/LCU.v
@@ -1,16 +1,16 @@
 `timescale 1ns / 1ps
 module LCU(
     input clk, rst,         //基本信号
-    input enq,deq,          //外部接口------------
+    input enq,deq,          //外部接口[
     input [3:0] in,
     output reg full,emp,
-    output [2:0] p,
-    output [3:0] out,
+    output [3:0] out,		//]
     output reg [7:0] valid, //SDU接口
-    output reg [2:0] ra,
+    output [2:0] p,		    //SDU拓展
+    output reg [2:0] ra,    //RF接口[
     input [3:0] rd,
     output reg [2:0] wa,
-    output reg [3:0] wd
+    output reg [3:0] wd	    //]
     );
     wire pulse_en,pulse_de;
     reg r1_en=0,r2_en=0;
@@ -28,13 +28,13 @@ module LCU(
     //构造单周期脉冲
     reg [2:0] RP=0,WP=0;
     wire [3:0] O;
-    reg [3:0] o;                //缓存历史数据(一层)
+    reg [3:0] o;                //备份历史数据
     assign p=RP;
     assign O=rd;
     assign out=full?o:O;        //该mux防止out输出被队将满时的入队数据覆盖
     always@(posedge clk)begin
         if(rst)begin
-            valid[7:0]=8'b0;    //reg0硬连线const0
+            valid[7:0]=8'b0;
             RP=0;
             WP=0;
             full=0;
@@ -55,7 +55,7 @@ module LCU(
             end
             if(pulse_de)begin
                 if(!emp)begin
-                    ra=RP;
+                    ra<=RP;
                     valid[RP]<=0;
                     RP=RP+1;
                     emp<=(RP==WP);
